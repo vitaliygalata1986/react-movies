f16f6cac63bf7ca10f89c3af05c2204aaa947c35
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-25 00:04:38 +0300

    modification with components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,190 +1,158 @@
 import React from 'react';
+import { useState } from 'react';
 
-class Search extends React.Component {
-  state = {
-    search: '',
-    type: 'all',
-    year: 2023,
-  };
+function Search({ searchMovies = Function.prototype }) {
+  const [search, setSearch] = useState('');
+  const [type, setType] = useState('all');
+  const [year, setYear] = useState(2023);
 
-  handleKey = (event) => {
+  const handleKey = (event) => {
     if (event.key === 'Enter') {
-      this.props.searchMovies(
-        this.state.search,
-        this.state.type,
-        this.state.year
-      );
+      searchMovies(search, type, year);
     }
   };
 
-  handleFilter = (event) => {
-    this.setState(
-      () => ({ type: event.target.value }),
-      () => {
-        this.props.searchMovies(
-          this.state.search,
-          this.state.type,
-          this.state.year
-        );
-      }
-    );
+  const handleFilter = (event) => {
+    setType(event.target.value);
+    searchMovies(search, event.target.value, year);
   };
 
-  handleYear = (event) => {
-    this.setState(
-      () => ({ year: +event.target.value }),
-      () => {
-        this.props.searchMovies(
-          this.state.search,
-          this.state.type,
-          this.state.year
-        );
-      }
-    );
+  const handleYear = (event) => {
+    setYear(+event.target.value);
+    searchMovies(search, type, event.target.value);
   };
 
-  render() {
-    const { type, year } = this.state;
-    return (
-      <div className="row">
-        <div className="col s12">
-          <div className="input-field">
-            <input
-              placeholder="Search ..."
-              type="search"
-              className="validate"
-              value={this.state.search}
-              onChange={(event) =>
-                this.setState({ search: event.target.value })
-              }
-              onKeyDown={this.handleKey}
-            />
-            <button
-              className="btn search-btn"
-              onClick={() =>
-                this.props.searchMovies(
-                  this.state.search,
-                  this.state.type,
-                  this.state.year
-                )
-              }
-            >
-              Search
-            </button>
-          </div>
-        </div>
-        <div className="col s12 block">
-          <span className="filters-radio">Genre:</span>
-          <div>
-            <label>
-              <input
-                className="with-gap"
-                name="filter_film"
-                value="all"
-                type="radio"
-                checked={type === 'all'}
-                onChange={this.handleFilter}
-              />
-              <span>All</span>
-            </label>
-            <label>
-              <input
-                className="with-gap"
-                name="filter_film"
-                value="movie"
-                type="radio"
-                checked={type === 'movie'}
-                onChange={this.handleFilter}
-              />
-              <span>Movies only</span>
-            </label>
-            <label>
-              <input
-                className="with-gap"
-                name="filter_film"
-                value="game"
-                type="radio"
-                checked={type === 'game'}
-                onChange={this.handleFilter}
-              />
-              <span>Game only</span>
-            </label>
-            <label>
-              <input
-                className="with-gap"
-                name="filter_film"
-                value="series"
-                type="radio"
-                checked={type === 'series'}
-                onChange={this.handleFilter}
-              />
-              <span>Series only</span>
-            </label>
-            <label>
-              <input
-                className="with-gap"
-                name="filter_film"
-                value="episode"
-                type="radio"
-                checked={type === 'episode'}
-                onChange={this.handleFilter}
-              />
-              <span>Episode only</span>
-            </label>
-          </div>
-        </div>
-        <div className="col s12 block">
-          <span className="filters-radio">Year:</span>
-          <div>
-            <label>
-              <input
-                className="with-gap"
-                name="year"
-                value="2023"
-                type="radio"
-                checked={year === 2023}
-                onChange={this.handleYear}
-              />
-              <span>2023</span>
-            </label>
-            <label>
-              <input
-                className="with-gap"
-                name="year"
-                value="2022"
-                type="radio"
-                checked={year === 2022}
-                onChange={this.handleYear}
-              />
-              <span>2022</span>
-            </label>
-            <label>
-              <input
-                className="with-gap"
-                name="year"
-                value="2021"
-                type="radio"
-                checked={year === 2021}
-                onChange={this.handleYear}
-              />
-              <span>2021</span>
-            </label>
-            <label>
-              <input
-                className="with-gap"
-                name="year"
-                value="2020"
-                type="radio"
-                checked={year === 2020}
-                onChange={this.handleYear}
-              />
-              <span>2020</span>
-            </label>
-          </div>
+  return (
+    <div className="row">
+      <div className="col s12">
+        <div className="input-field">
+          <input
+            placeholder="Search ..."
+            type="search"
+            className="validate"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            onKeyDown={handleKey}
+          />
+          <button
+            className="btn search-btn"
+            onClick={() => searchMovies(search, type, year)}
+          >
+            Search
+          </button>
         </div>
       </div>
-    );
-  }
+      <div className="col s12 block">
+        <span className="filters-radio">Genre:</span>
+        <div>
+          <label>
+            <input
+              className="with-gap"
+              name="filter_film"
+              value="all"
+              type="radio"
+              checked={type === 'all'}
+              onChange={handleFilter}
+            />
+            <span>All</span>
+          </label>
+          <label>
+            <input
+              className="with-gap"
+              name="filter_film"
+              value="movie"
+              type="radio"
+              checked={type === 'movie'}
+              onChange={handleFilter}
+            />
+            <span>Movies only</span>
+          </label>
+          <label>
+            <input
+              className="with-gap"
+              name="filter_film"
+              value="game"
+              type="radio"
+              checked={type === 'game'}
+              onChange={handleFilter}
+            />
+            <span>Game only</span>
+          </label>
+          <label>
+            <input
+              className="with-gap"
+              name="filter_film"
+              value="series"
+              type="radio"
+              checked={type === 'series'}
+              onChange={handleFilter}
+            />
+            <span>Series only</span>
+          </label>
+          <label>
+            <input
+              className="with-gap"
+              name="filter_film"
+              value="episode"
+              type="radio"
+              checked={type === 'episode'}
+              onChange={handleFilter}
+            />
+            <span>Episode only</span>
+          </label>
+        </div>
+      </div>
+      <div className="col s12 block">
+        <span className="filters-radio">Year:</span>
+        <div>
+          <label>
+            <input
+              className="with-gap"
+              name="year"
+              value="2023"
+              type="radio"
+              checked={year === 2023}
+              onChange={handleYear}
+            />
+            <span>2023</span>
+          </label>
+          <label>
+            <input
+              className="with-gap"
+              name="year"
+              value="2022"
+              type="radio"
+              checked={year === 2022}
+              onChange={handleYear}
+            />
+            <span>2022</span>
+          </label>
+          <label>
+            <input
+              className="with-gap"
+              name="year"
+              value="2021"
+              type="radio"
+              checked={year === 2021}
+              onChange={handleYear}
+            />
+            <span>2021</span>
+          </label>
+          <label>
+            <input
+              className="with-gap"
+              name="year"
+              value="2020"
+              type="radio"
+              checked={year === 2020}
+              onChange={handleYear}
+            />
+            <span>2020</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export { Search };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,50 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY; // обрабатывает все переменные в момент сборки
 
-class Main extends React.Component {
-  state = {
-    movies: [],
-    loading: true,
-  };
+function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=all&y=2023`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
       .catch((err) => {
         console.error(err);
-        this.setState({ loading: false });
+        setLoading(false);
       });
-  }
+  }, []);
 
-  searchMovies = (str, type = 'all', year) => {
-    this.setState({ loading: true });
+  const searchMovies = (str, type = 'all', year) => {
+    console.log('str=> ', str, 'type=> ', type, 'year=>', year);
+    setLoading(true);
     fetch(
       `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== 'all' ? `&type=${type}` : ''
       }&y=${year}`
     )
       .then((resonse) => resonse.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
       .catch((err) => {
         console.error(err);
-        this.setState({ loading: false });
+        setLoading(false);
       });
   };
 
-  render() {
-    const { movies, loading } = this.state;
-    return (
-      <main className="container content">
-        <Search searchMovies={this.searchMovies} />
-        {loading ? <Preloader /> : <Movies movies={movies} />}
-      </main>
-    );
-  }
+  return (
+    <main className="container content">
+      <Search searchMovies={searchMovies} />
+      {loading ? <Preloader /> : <Movies movies={movies} />}
+    </main>
+  );
 }
 
 export { Main };
